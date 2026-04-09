b9ee5d263c74fef741f44a071b80fd23bc79d2fe
orain <mugou5487@gmail.com>
2026-04-09 21:17:47 +0800

    prepare codes for backend

--- frontend/starmap_api.py
+++ frontend/starmap_api.py
@@ -7,6 +7,8 @@ Created on Thu Apr  9 14:43:22 2026
 
 import streamlit as st
 import numpy as np
+#from backend.py import get_star_position
+
 # My first app
 st.title("星空地圖")
 st.write("選擇觀測**時間** (24小時制)")
@@ -26,6 +28,8 @@ st.write("""選擇觀測**地點**""")
 loca = [st.text_input("國家"),st.text_input("縣市"),st.text_input("鄉鎮市區"),st.text_input("其他(選填)")]
 
 if st.button("go!"):
+    #pos = get_star_position(hr, mins)
     st.write("我啥都還沒開始寫，但我可以告訴你你輸入了",hr,"點",mins,"分")
     st.write(loca[0],loca[1],loca[2])
+    #st.write(pos)
 
