b0c3d49145b26a85ec0e683911cda23e4df30146
orain <mugou5487@gmail.com>
2026-04-09 22:15:04 +0800

    again

--- frontend/starmap_api.py
+++ frontend/starmap_api.py
@@ -39,7 +39,3 @@ contents = file_.read()
 data_url = base64.b64encode(contents).decode("utf-8")
 file_.close()
 
-st.markdown(
-    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-    unsafe_allow_html=True,
-    )
